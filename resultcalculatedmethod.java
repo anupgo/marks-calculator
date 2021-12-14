2f3efd4350a9163f6bb71af3ff1c61f5817baa99
anupgo <96070971+anupgo@users.noreply.github.com>
2021-12-14 17:18:52 +0530

    Create resultcalculatedmethod.java
    Instead of using loops in all subject. I have made a method and put that loop in that method.

---
+++ resultcalculatedmethod.java
@@ -0,0 +1,76 @@
+package com.company;
+import java.util.Scanner;
+
+
+public class javatrytodomethord {
+
+    public static class subject{
+
+
+    }
+
+    static void pass(int sub){
+
+        if (sub<33) {
+            System.out.println("failed");
+        }
+        else {
+            System.out.println("Passed");
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your subject 1 number out of 100 :");
+        int  sub1= sc.nextInt();
+        pass( sub1);
+
+        // Subject 2
+
+        System.out.print("Enter your subject 2 number out of 100 :");
+        int  sub2= sc.nextInt();
+        pass(sub2);
+
+        // Subject 3
+
+        System.out.print("Enter your subject 3 number out of 100 :");
+        int  sub3= sc.nextInt();
+        pass(sub3);
+
+        // Subject 4
+
+        System.out.print("Enter your subject 4 number out of 100 :");
+        int  sub4= sc.nextInt();
+        pass(sub4);
+
+        //Subject 5
+
+        System.out.print("Enter your subject 5 number out of 100 :");
+        int  sub5= sc.nextInt();
+        pass(sub5);
+
+        // total marks
+        int sum= sub1 + sub2 +sub3 +sub4+ sub5;
+        System.out.println("Your total marks out of 500 is : " + sum );
+
+        // total percentage
+
+        int per = sum /5;
+
+        System.out.println("Your percentage is : " + per + "%");
+
+        // over all pass/fail
+        if(sum >=165){
+            System.out.println("You have passed");
+        }
+        else {
+            System.out.println("try again you are fail");
+        }
+
+        //overall pass or fall
+
+
+
+
+    }
+}
